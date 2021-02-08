3aa159c2165958738d6a856e385ad681dc69b6c5
TowaAoyagi <tab.git.kamui@gmail.com>
2021-02-08 15:08:29 +0900

    Initial Commit

---
+++ whenWhoGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whenWhoGame
+//
+//  Created by Towa Aoyagi on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
